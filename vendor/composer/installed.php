e60bd8d9e4eaedfa7c80fcbdbc9e19c330edf2fe
José G. <jgil@ac-nice.fr>
2024-08-19 23:15:27 +0200

    Version 2025

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gsb/applifrais',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'be44ca092f8d64432ef3431b06be0f46baa50243',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gsb/applifrais' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'be44ca092f8d64432ef3431b06be0f46baa50243',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
